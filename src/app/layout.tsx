44e292bbaa5a5edebe9b6c4962f5e1a51cfeebf2
Jose Francisco Mateo Carrasco <locutermo@gmail.com>
2024-02-18 23:05:39 -0500

    add insights!

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from '@/components/header'
 import Footer from '@/components/footer'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -27,6 +28,7 @@ export default function RootLayout({
           {children}
         </main>
         <Footer/>
+        <SpeedInsights/>
       </body>   
     </html>
       
