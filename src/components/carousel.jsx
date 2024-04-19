984aa494ca8c60e1a746b52a92dcc2561316bf40
Jose Francisco Mateo Carrasco <jose.mateo@telefonica.com>
2024-04-19 02:48:31 -0500

    feat: Agregar estrellas en el carousel de hotel

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -12,6 +12,16 @@ export default function Carousel({ items = [] }) {
         setCurrentIndex((prevIndex) => prevIndex == 0 ? items.length - 1 : (prevIndex - 1));
     };
 
+    const showStars = (number) => {
+        let stars = [];
+        for (let i = 0; i < number; i++) {
+            stars.push(<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="w-6 h-6 ">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+            </svg>)
+        }
+        return stars;
+    }
+
     return (
         <div id="default-carousel" className="relative w-full" data-carousel="slide">
             <div className="relative overflow-hidden lg:rounded-lg h-80 md:min-h-[500px] lg:min-h-[700px] lg:max-h-[700px]">
@@ -26,6 +36,9 @@ export default function Carousel({ items = [] }) {
                         <div className="absolute z-20 w-full bg-black h-full left-0  opacity-40"></div>
                         <div className="absolute z-30 w-full h-full left-0">
                             <div className="flex w-full md:w-2/3 lg:w-1/2 h-full px-10 md:px-20 lg:px-32 flex-col justify-center md:space-y-2">
+                                <div className="flex">
+                                    {[...showStars(item.stars)]} 
+                                </div>
                                 <h1 className="text-white font-extrabold md:text-4xl lg:text-5xl py-1 md:py-0">{item.name}</h1>
                                 <p className="text-white text-xs md:text-base lg:text-sm">{item.description}</p>
                             </div>
@@ -58,7 +71,7 @@ export default function Carousel({ items = [] }) {
 }
 
 
-const CarouselButton = ({ children, handleClick,position }) => {
+const CarouselButton = ({ children, handleClick, position }) => {
     return <button type="button" onClick={handleClick} className={`absolute top-0 ${position}-0 z-30 flex items-center justify-center h-full px-1 md:px-4 cursor-pointer group focus:outline-none`}>
         <span className="inline-flex items-center justify-center w-6 h-6 md:w-10 md:h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
             {children}
