3bb215146cc9aa60dfe6d2b8b0bfd1acb13d4b4a
Jose Francisco Mateo Carrasco <jose.mateo@telefonica.com>
2024-04-29 02:05:41 -0500

    fix: corregir diseño

--- src/components/national-section.jsx
+++ src/components/national-section.jsx
@@ -9,10 +9,7 @@ export default async function NationalSection({ ...restProps }) {
         <p className="text-sm md:text-base text-center pb-14 py-4">Embárcate en una aventura por los increíbles destinos nacionales que el Perú tiene para ofrecerte</p>
         <div className="gap-4 mt-8 lg:gap-20 grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2">
             {destinations?.map((destination, index) => (
-                <div className={`${ index==(destinations.length-1) &&'lg:col-start-2' }`}>
-
-                    <FlipCard size={"md"} key={destination._id} {...destination} />
-                </div >
+                <FlipCard size={"md"} key={destination._id} {...destination} />
             ))}
         </div>
     </section>
