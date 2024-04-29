3001e3eca8194dcb556e00ccd5c57fa39cf202b6
Jose Francisco Mateo Carrasco <jose.mateo@telefonica.com>
2024-04-28 21:36:46 -0500

    hide sections

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,8 +11,7 @@ export default async function Home(props) {
         <HeaderMedia />
       </Suspense>
       <InternationalSection />
-      <NationalSection />
-      <AboutUs />
+     
 
     </main>
   );
