af3745114ae8e112be7287c983fed4db2c3c4382
Jose Francisco Mateo Carrasco <jose.mateo@telefonica.com>
2024-04-29 02:35:35 -0500

    feat: Agregar nuevos destinos nacionales

--- src/components/national-section.jsx
+++ src/components/national-section.jsx
@@ -7,7 +7,7 @@ export default async function NationalSection({ ...restProps }) {
     return <section id="national-destination" className="py-8 pb-20 px-4 md:px-20 xl:px-[10%] flex z-10 flex-col w-full justify-center lg:min-h-screen">
         <h3 className="text-xl font-bold md:text-3xl text-center pt-14 text-[#4563ff] uppercase">Destinos Nacionales</h3>
         <p className="text-sm md:text-base text-center pb-14 py-4">Embárcate en una aventura por los increíbles destinos nacionales que el Perú tiene para ofrecerte</p>
-        <div className="gap-4 mt-8 lg:gap-20 grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2">
+        <div className="gap-4 mt-8 lg:gap-10 grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-3">
             {destinations?.map((destination, index) => (
                 <FlipCard size={"md"} key={destination._id} {...destination} />
             ))}
